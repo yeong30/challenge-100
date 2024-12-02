68d78a74229661795eee500765a99583eeb1512d
uuyeong <proyeongs@gmail.com>
2024-12-02 21:35:39 +0900

    chore: Tailwind 초기 설정

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#fcba03',
+      },
+    },
+  },
+  plugins: [],
+};
